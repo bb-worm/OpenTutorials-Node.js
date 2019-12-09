8fbf53ebee14f964e7ee7392a7936180681f8631
현태룡 <xofyd99@naver.com>
2019-12-09 22:24:47 +0900

    make object to module

--- main.js
+++ main.js
@@ -2,34 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-
-const template = {
-  html: (title, list, body, control) => {
-    return `<!doctype html>
-    <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}
-        ${control === undefined ? '' : control}
-        ${body}
-      </body>
-    </html>`;
-  },
-
-  list: (filelist) => {
-    let list = `<ul>`;
-    for (let i=0; i< filelist.length; i++){
-      list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-    }
-    list += `</ul>`;
-    
-    return list;
-  }
-}
+const template = require('./lib/template.js');
 
 var app = http.createServer((request, response) => {
     var _url = request.url;
