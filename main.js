82a39c2072f7ceb3c273894050861a4a4e8f2697
현태룡 <xofyd99@naver.com>
2019-12-10 20:39:38 +0900

    express - change url form

--- main.js
+++ main.js
@@ -36,14 +36,92 @@ app.get('/', (request, response) => {
   const list = template.list(request.list); // template 모듈 사용
   const body = `<h2>${title}</h2><p>${description}</p>
   <img src="/images/music.jpg" style="width:500px;" />`;
-  const control = `<a href="/create">create</a>`;
+  const control = `<a href="/topic/create">create</a>`;
   const html = template.html(title, list, body, control); // template 모듈 사용
   
   response.send(html);
 })
 
+app.get('/topic/create', (request, response) => {
+  const title = 'WEB - create';
+  const list = template.list(request.list); // template 모듈 사용
+  const body = `
+  <form action="/topic/create_process" method="POST">
+    <p><input type="text" name="title" placeholder="title" /></p>
+    <p>
+        <textarea name="description" placeholder="description"></textarea>
+    </p>
+    <p>
+        <input type="submit" />
+    </p>
+  </form>`;
+  const html = template.html(title, list, body); // template 모듈 사용
+  
+  response.send(html);
+})
+
+app.post('/topic/create_process', (request, response) => {
+  // bodyparser middleware를 통해 request.on()을 할 필요 없이 body를 가져옴
+  const post = request.body;
+  const title = post.title;
+  const description = post.description;
+  
+  fs.writeFile(`data/${title}`, description, 'utf-8', err => {
+    response.redirect(`/topic/${qs.escape(title)}`);
+  })
+})
+
+app.get('/topic/update/:pageId', (request, response, next) => {
+  const filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, 'utf-8', (err, description) => {
+      if (err){
+        // status code 500
+        next(err);
+      }
+      else{
+        const title = request.params.pageId;
+      const list = template.list(request.list); // template 모듈 사용
+      const body = `
+      <form action="/topic/update_process" method="POST">
+        <input type="hidden" name="id" value="${title}" />
+        <p><input type="text" name="title" placeholder="title" value="${title}" /></p>
+        <p><textarea name="description" placeholder="description">${description}</textarea></p>
+        <p><input type="submit" /></p>
+      </form>
+      `;
+      
+      const html = template.html(title, list, body); // template 모듈 사용
+
+      response.send(html);
+      }
+    })
+})
+
+app.post('/topic/update_process', (request, response) => {
+  const post = request.body;
+  const id = post.id;
+  const title = post.title;
+  const description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, (err) => {
+    console.log(err);
+  });
+  fs.writeFile(`data/${title}`, description, 'utf-8', err => {
+    response.redirect(`/topic/${qs.escape(title)}`)
+  })
+})
+
+app.post('/topic/delete_process', (request, response) => {
+  const post = request.body;
+  const id = post.id;
+  const filteredId = path.parse(id).base;
+
+  fs.unlink(`data/${filteredId}`, (err) => {
+    response.redirect('/');
+  })
+})
+
 // querystring 사용하지 않고, route parameter를 사용
-app.get('/page/:pageId', (request, response, next) => {
+app.get('/topic/:pageId', (request, response, next) => {
   const filteredId = path.parse(request.params.pageId).base; // queryData.id가 ..을 사용해서 상위 dir로 가는 것을 막을 수 있음
   fs.readFile(`data/${filteredId}`, 'utf-8', (err, description) => {
     if (err){
@@ -61,9 +139,9 @@ app.get('/page/:pageId', (request, response, next) => {
       const body = `<h2>${title}</h2><p>${sanitizedDescription}</p>`;
   
       // delete는 link를 사용하는 get으로 구현해서는 안 됨. 접근할 수 없도록 post 방식으로 보내야 함.
-      const control = `<a href="/create">create</a>
-      <a href="/update/${sanitizedTitle}">update</a>
-      <form action="/delete_process" method="POST">
+      const control = `<a href="/topic/create">create</a>
+      <a href="/topic/update/${sanitizedTitle}">update</a>
+      <form action="/topic/delete_process" method="POST">
         <input type="hidden" name="id" value="${sanitizedTitle}" />
         <input type="submit" value="delete" />
       </form>`;
@@ -71,85 +149,6 @@ app.get('/page/:pageId', (request, response, next) => {
   
       response.send(html);
     }
-  })
-})
-
-app.get('/create', (request, response) => {
-  const title = 'WEB - create';
-  const list = template.list(request.list); // template 모듈 사용
-  const body = `
-  <form action="/create_process" method="POST">
-    <p><input type="text" name="title" placeholder="title" /></p>
-    <p>
-        <textarea name="description" placeholder="description"></textarea>
-    </p>
-    <p>
-        <input type="submit" />
-    </p>
-  </form>`;
-  const html = template.html(title, list, body); // template 모듈 사용
-  
-  response.send(html);
-
-})
-
-app.post('/create_process', (request, response) => {
-  // bodyparser middleware를 통해 request.on()을 할 필요 없이 body를 가져옴
-  const post = request.body;
-  const title = post.title;
-  const description = post.description;
-  
-  fs.writeFile(`data/${title}`, description, 'utf-8', err => {
-    response.redirect(`/page/${qs.escape(title)}`);
-  })
-})
-
-app.get('/update/:pageId', (request, response, next) => {
-  const filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf-8', (err, description) => {
-      if (err){
-        // status code 500
-        next(err);
-      }
-      else{
-        const title = request.params.pageId;
-      const list = template.list(request.list); // template 모듈 사용
-      const body = `
-      <form action="/update_process" method="POST">
-        <input type="hidden" name="id" value="${title}" />
-        <p><input type="text" name="title" placeholder="title" value="${title}" /></p>
-        <p><textarea name="description" placeholder="description">${description}</textarea></p>
-        <p><input type="submit" /></p>
-      </form>
-      `;
-      
-      const html = template.html(title, list, body); // template 모듈 사용
-
-      response.send(html);
-      }
-    })
-})
-
-app.post('/update_process', (request, response) => {
-  const post = request.body;
-  const id = post.id;
-  const title = post.title;
-  const description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, (err) => {
-    console.log(err);
-  });
-  fs.writeFile(`data/${title}`, description, 'utf-8', err => {
-    response.redirect(`/page/${qs.escape(title)}`)
-  })
-})
-
-app.post('/delete_process', (request, response) => {
-  const post = request.body;
-  const id = post.id;
-  const filteredId = path.parse(id).base;
-
-  fs.unlink(`data/${filteredId}`, (err) => {
-    response.redirect('/');
   })
 })
 
