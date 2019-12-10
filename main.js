b40ea74b0d5d32f1a5e634af425bef253a719c16
현태룡 <xofyd99@naver.com>
2019-12-10 16:08:53 +0900

    express - make middleware

--- main.js
+++ main.js
@@ -6,76 +6,80 @@ const qs = require('querystring');
 const sanitizeHtml = require('sanitize-html');
 const template = require('./lib/template');
 
+const port = 3000
+
 // middleware 
 const bodyParser = require('body-parser');
 const compression = require('compression');
-
-const port = 3000
 
 // middleware 장착
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
 
+app.use((request, response, next) => {
+  fs.readdir('./data', 'utf-8', (err, filelist) => {
+    request.list = filelist;
+    next();
+  })
+})
+
 // Home
 app.get('/', (request, response) => {
-  fs.readdir('./data', 'utf-8', (err, filelist) => {
-    const description = 'Hello, Node.js';
-    const title = 'Welcome';
-    const list = template.list(filelist); // template 모듈 사용
-    const body = `<h2>${title}</h2><p>${description}</p>`;
-    const control = `<a href="/create">create</a>`;
-    const html = template.html(title, list, body, control); // template 모듈 사용
-    
-    response.send(html);
-  })
+  console.log(request.list);
+
+  const description = 'Hello, Node.js';
+  const title = 'Welcome';
+  const list = template.list(request.list); // template 모듈 사용
+  const body = `<h2>${title}</h2><p>${description}</p>`;
+  const control = `<a href="/create">create</a>`;
+  const html = template.html(title, list, body, control); // template 모듈 사용
+  
+  response.send(html);
 })
 
 // querystring 사용하지 않고, route parameter를 사용
 app.get('/page/:pageId', (request, response) => {
   const filteredId = path.parse(request.params.pageId).base; // queryData.id가 ..을 사용해서 상위 dir로 가는 것을 막을 수 있음
   fs.readFile(`data/${filteredId}`, 'utf-8', (err, description) => {
-    fs.readdir('./data', 'utf-8', (err, filelist) => {
-      const title = request.params.pageId;
-      const sanitizedTitle = sanitizeHtml(title);
-      const sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
+    const title = request.params.pageId;
+    const sanitizedTitle = sanitizeHtml(title);
+    const sanitizedDescription = sanitizeHtml(description, {
+      allowedTags: ['h1']
+    });
 
-      const list = template.list(filelist); // template 모듈 사용
-      const body = `<h2>${title}</h2><p>${sanitizedDescription}</p>`;
+    const list = template.list(request.list); // template 모듈 사용
+    const body = `<h2>${title}</h2><p>${sanitizedDescription}</p>`;
 
-      // delete는 link를 사용하는 get으로 구현해서는 안 됨. 접근할 수 없도록 post 방식으로 보내야 함.
-      const control = `<a href="/create">create</a>
-      <a href="/update/${sanitizedTitle}">update</a>
-      <form action="/delete_process" method="POST">
-        <input type="hidden" name="id" value="${sanitizedTitle}" />
-        <input type="submit" value="delete" />
-      </form>`;
-      const html = template.html(sanitizedTitle, list, body, control); // template 모듈 사용
+    // delete는 link를 사용하는 get으로 구현해서는 안 됨. 접근할 수 없도록 post 방식으로 보내야 함.
+    const control = `<a href="/create">create</a>
+    <a href="/update/${sanitizedTitle}">update</a>
+    <form action="/delete_process" method="POST">
+      <input type="hidden" name="id" value="${sanitizedTitle}" />
+      <input type="submit" value="delete" />
+    </form>`;
+    const html = template.html(sanitizedTitle, list, body, control); // template 모듈 사용
 
-      response.send(html);
-    })
+    response.send(html);
   })
 })
 
 app.get('/create', (request, response) => {
-  fs.readdir('./data', 'utf-8', (err, filelist) => {
-    const title = 'WEB - create';
-    const list = template.list(filelist); // template 모듈 사용
-    const body = `
-    <form action="/create_process" method="POST">
-      <p><input type="text" name="title" placeholder="title" /></p>
-      <p>
-          <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-          <input type="submit" />
-      </p>
-    </form>`;
-    const html = template.html(title, list, body); // template 모듈 사용
-    
-    response.send(html);
-  })
+  const title = 'WEB - create';
+  const list = template.list(request.list); // template 모듈 사용
+  const body = `
+  <form action="/create_process" method="POST">
+    <p><input type="text" name="title" placeholder="title" /></p>
+    <p>
+        <textarea name="description" placeholder="description"></textarea>
+    </p>
+    <p>
+        <input type="submit" />
+    </p>
+  </form>`;
+  const html = template.html(title, list, body); // template 모듈 사용
+  
+  response.send(html);
+
 })
 
 app.post('/create_process', (request, response) => {
@@ -92,9 +96,8 @@ app.post('/create_process', (request, response) => {
 app.get('/update/:pageId', (request, response) => {
   const filteredId = path.parse(request.params.pageId).base;
     fs.readFile(`data/${filteredId}`, 'utf-8', (err, description) => {
-      fs.readdir('./data', 'utf-8', (err, filelist) => {
       const title = request.params.pageId;
-      const list = template.list(filelist); // template 모듈 사용
+      const list = template.list(request.list); // template 모듈 사용
       const body = `
       <form action="/update_process" method="POST">
         <input type="hidden" name="id" value="${title}" />
@@ -107,7 +110,6 @@ app.get('/update/:pageId', (request, response) => {
       const html = template.html(title, list, body); // template 모듈 사용
 
       response.send(html);
-      })
     })
 })
 
