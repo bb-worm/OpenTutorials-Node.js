631fff95029daf70817c0e02157eed2f6b994e06
현태룡 <xofyd99@naver.com>
2019-12-11 19:36:18 +0900

    cookie - path and domain options

--- test/cookieTest/cookie.js
+++ test/cookieTest/cookie.js
@@ -13,7 +13,9 @@ http.createServer((request, response) => {
             'tasty_cookie=strawberry',
             `Permanent=cookies; Max-Age=${60*60*24*30}`,
             'Secure_cookie=cookies; Secure', // https로 통신하는 경우에만 웹브라우저가 쿠키를 서버로 전송
-            'Http_cookie=cookies; HttpOnly'] // javacript의 document.cookie로 사용되는 것을 막음
+            'Http_cookie=cookies; HttpOnly', // javacript의 document.cookie로 사용되는 것을 막음
+            'Path_cookie=cookies; Path=/cookie', // 특정 path에서만 웹브라우저가 쿠키를 서버로 전송
+            'Domain_cookies=cookies; Domain=o2.org'] // o2.org 앞에 어떤 sub domain이 오더라도 전송됨
     });
 
     response.end('Cookie!');
