b14d47e9d17baa14fb2da8a0f3e31ab3444cfbe9
현태룡 <xofyd99@naver.com>
2019-12-11 19:24:22 +0900

    cookie - secure and httponly options

--- test/cookieTest/cookie.js
+++ test/cookieTest/cookie.js
@@ -11,7 +11,9 @@ http.createServer((request, response) => {
         'Set-Cookie': [
             'yummy_cookie=choco',
             'tasty_cookie=strawberry',
-            `Permanent=cookies; Max-Age=${60*60*24*30}`]
+            `Permanent=cookies; Max-Age=${60*60*24*30}`,
+            'Secure_cookie=cookies; Secure', // https로 통신하는 경우에만 웹브라우저가 쿠키를 서버로 전송
+            'Http_cookie=cookies; HttpOnly'] // javacript의 document.cookie로 사용되는 것을 막음
     });
 
     response.end('Cookie!');
