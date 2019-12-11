5696e8fde5117f1e57104ca01c329b29e388d176
현태룡 <xofyd99@naver.com>
2019-12-11 19:18:20 +0900

    cookie - set cookie expiring time

--- test/cookieTest/cookie.js
+++ test/cookieTest/cookie.js
@@ -8,7 +8,10 @@ http.createServer((request, response) => {
         console.log(cookie.parse(coo));
 
     response.writeHead(200, {
-        'Set-Cookie': ['yummy_cookie=choco','tasty_cookie=strawberry']
+        'Set-Cookie': [
+            'yummy_cookie=choco',
+            'tasty_cookie=strawberry',
+            `Permanent=cookies; Max-Age=${60*60*24*30}`]
     });
 
     response.end('Cookie!');
