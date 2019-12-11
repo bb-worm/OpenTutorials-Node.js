79fca1e55a35d02795244cc0365054cc08354a81
현태룡 <xofyd99@naver.com>
2019-12-11 15:44:47 +0900

    cookie - install cookie module and read cookie

--- test/cookieTest/cookie.js
+++ test/cookieTest/cookie.js
@@ -1,8 +1,14 @@
 const http = require('http');
+const cookie = require('cookie');
+
 http.createServer((request, response) => {
 
+    const coo = request.headers.cookie;
+    if (coo !== undefined)
+        console.log(cookie.parse(coo));
+
     response.writeHead(200, {
-        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+        'Set-Cookie': ['yummy_cookie=choco','tasty_cookie=strawberry']
     });
 
     response.end('Cookie!');
