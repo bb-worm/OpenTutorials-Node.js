d535770a7f966ed5deb11eea05a6eb1d0aef9f85
현태룡 <xofyd99@naver.com>
2019-12-23 18:58:44 +0900

    session - use session store

--- test/express-session.js
+++ test/express-session.js
@@ -1,28 +1,29 @@
-var express = require('express')
-var parseurl = require('parseurl')
-var session = require('express-session')
+var express = require("express");
+var parseurl = require("parseurl");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
 
-var app = express()
+var app = express();
 
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: new FileStore()
+  })
+);
 
-app.get('/', function (req, res, next) {
-    console.log(req.session);
+app.get("/", function(req, res, next) {
+  console.log(req.session);
 
-    if (req.session.num === undefined){
-        req.session.num = 1;
-    }
-    else {
-        req.session.num += 1;
-    }
+  if (req.session.num === undefined) {
+    req.session.num = 1;
+  } else {
+    req.session.num += 1;
+  }
 
-    res.send(`Views : ${req.session.num}`);
-})
-
-app.listen(3000, () => {
-
+  res.send(`Views : ${req.session.num}`);
 });
+
+app.listen(3000, () => {});
